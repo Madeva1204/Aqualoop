25b361dc85ef5e776bd3df569ec8dad4feec4796
Madeva1204 <mayangdevanidwinanda@gmail.com>
2026-05-12 09:08:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,11 @@ st.write(
 )
 import streamlit as st
 
-st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+a, b = st.columns(2)
+c, d = st.columns(2)
+
+a.metric("Temperature", "30°F", "-9°F", border=True)
+b.metric("Wind", "4 mph", "2 mph", border=True)
+
+c.metric("Humidity", "77%", "5%", border=True)
+d.metric("Pressure", "30.34 inHg", "-2 inHg", border=True)
