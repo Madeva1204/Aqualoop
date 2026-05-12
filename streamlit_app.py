9b4e46e5df2b12ea614f74adecd0e3a63884a9e8
Madeva1204 <mayangdevanidwinanda@gmail.com>
2026-05-12 09:15:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,9 @@
 import streamlit as st
 
-st.title("🎈 Aqualoop")
+st.title("🎈 happy birthday cila")
 st.write(
-    "Ayo kita mulai mendeteksi tangki! [docs.streamlit.io](https://docs.streamlit.io/)."
+    "ciee 20 tahun! [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-import streamlit as st
-
-a, b = st.columns(2)
-c, d = st.columns(2)
-
-a.metric("Temperature", "30°F", "-9°F", border=True)
-b.metric("Wind", "4 mph", "2 mph", border=True)
-
-c.metric("Humidity", "77%", "5%", border=True)
-d.metric("Pressure", "30.34 inHg", "-2 inHg", border=True)
 import streamlit as st
 
 st.balloons()
