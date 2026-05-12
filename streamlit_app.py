d88be0163aa9cda0dd4a8e5a4b46e40704609b7d
Madeva1204 <mayangdevanidwinanda@gmail.com>
2026-05-12 09:07:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("🎈 Aqualoop")
 st.write(
     "Ayo kita mulai mendeteksi tangki! [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
